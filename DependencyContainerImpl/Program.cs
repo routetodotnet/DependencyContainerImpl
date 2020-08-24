a1f71c1d52c9a27bb05d57a39e99d0ad997b30c4
routetodotnet <routetodotnet@gmail.com>
2020-08-23 23:41:59 -0400

    Dependency container resolver

--- DependencyContainerImpl/Program.cs
+++ DependencyContainerImpl/Program.cs
@@ -11,16 +11,26 @@ namespace DependencyContainerImpl
             Console.WriteLine("Hello World!");
             DependencyContainer container = new DependencyContainer();
             container.Add(typeof(IPersonRep), typeof(PersonRepository));
+            foreach (var item in container.dicContainer)
+            {
+                Console.WriteLine($"{ item.Key}--{item.Value} ");
+
+            }
 
             var typeinfo = typeof(PersonController);
             var constructor = typeinfo.GetConstructors().Single();
             var paratype= constructor.GetParameters().Select(a => a.ParameterType).Single();
-            Console.WriteLine(paratype.Name);
-            var resolvertype=container.GetTypeInfo(paratype.Name.GetType());
-            var resolverobject=container.dicContainer.FirstOrDefault(a => a.Key == resolvertype);
+            Console.WriteLine(paratype);
+            var dicTypeObject = container.dicContainer.FirstOrDefault(a => a.Key.Equals(paratype));
+            Console.WriteLine(dicTypeObject.Value.UnderlyingSystemType);
 
-          //  var personRepo = (PersonRepository)Activator.CreateInstance(typeof(PersonRepository));
-            var personcontroller = (PersonController)Activator.CreateInstance(typeof(PersonController),(resolverobject));
+           var resolvertype= dicTypeObject.Value.UnderlyingSystemType;
+
+            Console.WriteLine(resolvertype);
+           //var resolverobject=container.dicContainer.FirstOrDefault(a =>a.Key== paratype.Name);
+
+           var personRepo = (PersonRepository)Activator.CreateInstance(resolvertype);
+            var personcontroller = (PersonController)Activator.CreateInstance(typeof(PersonController),personRepo);
             personcontroller.DisaplyPerson();
 
             Console.ReadLine();
@@ -31,13 +41,13 @@ namespace DependencyContainerImpl
 
     public class DependencyContainer
     {
-        public Dictionary<Type, Type> dicContainer;
+        public Dictionary<Type , Type> dicContainer;
         public DependencyContainer()
         {
             dicContainer = new Dictionary<Type, Type>();
 
         }
-        public void Add(Type tkey,Type tvalue)
+        public void Add(Type tkey, Type tvalue)
         {
             dicContainer.Add(tkey, tvalue);
 
@@ -45,7 +55,7 @@ namespace DependencyContainerImpl
         public Type GetTypeInfo(Type tkey)
         {
 
-            return dicContainer[tkey];
+            return dicContainer[tkey].GetType();
         }
 
     }
