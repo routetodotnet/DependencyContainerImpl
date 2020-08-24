181e571d840930b7dddffdbfe470994b8a3f24cc
routetodotnet <routetodotnet@gmail.com>
2020-08-23 23:03:31 -0400

    generic container

--- DependencyContainerImpl/Program.cs
+++ DependencyContainerImpl/Program.cs
@@ -9,15 +9,18 @@ namespace DependencyContainerImpl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            DependencyContainer container = new DependencyContainer();
+            container.Add(typeof(IPersonRep), typeof(PersonRepository));
+
             var typeinfo = typeof(PersonController);
             var constructor = typeinfo.GetConstructors().Single();
             var paratype= constructor.GetParameters().Select(a => a.ParameterType).Single();
+            Console.WriteLine(paratype.Name);
+            var resolvertype=container.GetTypeInfo(paratype.Name.GetType());
+            var resolverobject=container.dicContainer.FirstOrDefault(a => a.Key == resolvertype);
 
-
-            Console.WriteLine(paratype);
-
-            var personRepo = (PersonRepository)Activator.CreateInstance(typeof(PersonRepository));
-            var personcontroller = (PersonController)Activator.CreateInstance(typeof(PersonController), personRepo);
+          //  var personRepo = (PersonRepository)Activator.CreateInstance(typeof(PersonRepository));
+            var personcontroller = (PersonController)Activator.CreateInstance(typeof(PersonController),(resolverobject));
             personcontroller.DisaplyPerson();
 
             Console.ReadLine();
@@ -28,8 +31,23 @@ namespace DependencyContainerImpl
 
     public class DependencyContainer
     {
+        public Dictionary<Type, Type> dicContainer;
+        public DependencyContainer()
+        {
+            dicContainer = new Dictionary<Type, Type>();
 
-        
+        }
+        public void Add(Type tkey,Type tvalue)
+        {
+            dicContainer.Add(tkey, tvalue);
+
+        }
+        public Type GetTypeInfo(Type tkey)
+        {
+
+            return dicContainer[tkey];
+        }
+
     }
     public interface IPersonRep
     {
